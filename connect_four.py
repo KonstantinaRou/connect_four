92c0f62847695adc16aae1452b126f806ab6c493
krousia <konstantina.rou@macbookprorou.dyndns.cern.ch>
2018-11-16 13:58:04 +0100

    edit: Remove Board from being aclass

--- connect_four.py
+++ connect_four.py
@@ -5,13 +5,13 @@ def start_game():
     while board is "RETRY":
         print("You entered wrong input, please enter only numbers bigger than 0")
         board = initialize_board()
-    print(board.board)
+    print(board)
     player1 = Player('R')
     player2 = Player('G')
     player1.move(board)
     player1.move(board)
     player1.move(board)
-    print (board.board)
+    print (board)
 
 
 def initialize_board():
@@ -21,31 +21,22 @@ def initialize_board():
             return "RETRY"
 
         grids = [['X'] * x for _ in range(y)]
-        return Board(x, y, grids)
+        return grids
 
     except ValueError:
         return "RETRY"
 
-class Board:
-    # every board has specific size
-    def __init__(self, x, y, board):
-        self.x = x
-        self.y = y
-        self.board = board
-
 
 class Player:
-
     def __init__(self, color):
         # every player has a color idendity
         self.color = color
 
     def move(self, board):
-        board = board.board
         # make a move on the board, place the player's color inside the board
         column = int(input(" Place your disk at: (enter column)"))
         # check if column is out of bounds
-        while column > len(board) or column < 0:
+        while column > len(board) or column <= 0:
             print("Wrong input, try again")
             column = int(input("Place your disk at: (enter column)"))
         while board[0][column - 1] != 'X':
