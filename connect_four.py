d696c96c953c3fe5d3da58186cdd676b3262def1
krousia <konstantina.rou@macbookprorou.dyndns.cern.ch>
2018-11-16 12:11:03 +0100

    feat: Create Player class and Board class

--- connect_four.py
+++ connect_four.py
@@ -5,6 +5,13 @@ def start_game():
     while board is "RETRY":
         print("You entered wrong input, please enter only numbers bigger than 0")
         board = initialize_board()
+    print(board.board)
+    player1 = Player('R')
+    player2 = Player('G')
+    player1.move(board.board)
+    player1.move(board.board)
+    player1.move(board.board)
+    print (board.board)
 
 
 def initialize_board():
@@ -13,11 +20,46 @@ def initialize_board():
         if x <= 0 or y <= 0:
             return "RETRY"
 
-        grids = [[0] * x for _ in range(y)]
-        return grids
+        grids = [['X'] * x for _ in range(y)]
+        return Board(x, y, grids)
 
     except ValueError:
         return "RETRY"
+
+class Board:
+    # every board has specific size
+    def __init__(self, x, y, board):
+        self.x = x
+        self.y = y
+        self.board = board
+
+
+class Player:
+
+    def __init__(self, color):
+        # every player has a color idendity
+        self.color = color
+
+    def move(self, board):
+        # make a move on the board, place the player's color inside the board
+        column = int(input(" Place your disk at: (enter column)"))
+        # check if column is out of bounds
+        while column > len(board) or column <0:
+            print("Wrong input, try again")
+            column = input("Place your disk at: (enter column)")
+        while board[0][column - 1] != 'X':
+            print('slot', column, 'is full try again')
+            column = input("Place your disk at: (enter column)")
+        else:
+            # piece the disk inside the board
+            for slot in board[::-1]:
+                if slot[column] == 'X':
+                    slot[column] = self.color
+                    return board
+
+
+
+
 
 
 if __name__ == '__main__':
